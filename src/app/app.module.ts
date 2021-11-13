933f9105a8b2769b1d89dce1cc8a824f48b6e54c
Ivan <ivan.zf.netc@gmail.com>
2021-11-13 19:38:47 +0200

    Added the TreeGrid with testing data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FilterService, PageService, SortService, TreeGridModule } from '@syncfusion/ej2-angular-treegrid';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +9,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TreeGridModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [PageService,
+              SortService,
+              FilterService]
 })
 export class AppModule { }
