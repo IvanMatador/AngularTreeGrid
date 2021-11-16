7b58e51d11d1fdf7686dbfeaf1d36e2412e3cf2e
Ivan Raznatovskyi <ivan.raznatovskyi@incode-it.com>
2021-11-16 17:35:06 +0200

    Add context menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { dataSource, virtualData } from './datasource';
 import { VirtualScrollService, TreeGridComponent, ColumnMenuService, EditSettingsModel, ResizeService, EditService, ContextMenuService } from '@syncfusion/ej2-angular-treegrid';
+import { Dialog } from '@syncfusion/ej2-popups';
+import { DialogComponent } from '@syncfusion/ej2-angular-popups';
+import { EmitType } from '@syncfusion/ej2-base';
+import { ButtonModel } from '@syncfusion/ej2-angular-buttons';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +13,10 @@ import { VirtualScrollService, TreeGridComponent, ColumnMenuService, EditSetting
   styleUrls: ['./app.component.css'],
   providers: [VirtualScrollService, ColumnMenuService, ResizeService, EditService, ContextMenuService]
 })
+
 export class AppComponent implements OnInit {
   title = 'Angular-TreeGrid';
+
   @ViewChild('treegrid') treegrid!: TreeGridComponent;
   public data!: Object[];
   public contextMenuItems!: Object;
@@ -17,44 +24,87 @@ export class AppComponent implements OnInit {
   public toolbar!: string[];
   public editparams!: Object;
   public targetElement!: HTMLElement;
+  @ViewChild('ejDialog') ejDialog!: DialogComponent;
+  @ViewChild('container', { read: ElementRef, static: true }) container!: ElementRef;
+  public buttons: Object[] = [
+    { buttonModel: { content: 'Cancel', isPrimary: true, cssClass:'e-danger' } },
+    { buttonModel: { content: 'OK', isPrimary: false, cssClass:'e-success' } },
+  ];
+  public dialogHeaderText = 'Header';
+  public dialogContentText = 'Content';
+
+  public formControl = new FormControl();
 
   ngOnInit(): void {
+    this.initilaizeTarget();
     dataSource();
     this.data = virtualData;
     this.contextMenuItems= [
-      {text: 'Edit Column', target: '.e-headercontent', id: 'EditCol'},
-      {text: 'New Column', target: '.e-headercontent', id: 'NewCol'},
+      { text: 'Edit Column', target: '.e-headercontent', id: 'EditCol' },
+      { text: 'New Column', target: '.e-headercontent', id: 'NewCol' },
       { text: 'Delete Column', target: '.e-headercontent', id: 'DeleteCol' },
       { text: 'Choose Colomn', target: '.e-headercontent', id: 'ChooseCol' },
       { text: 'Freez Column', target: '.e-headercontent', id: 'FreezCol' },
       { text: 'Filter Column', target: '.e-headercontent', id: 'FilterCol' },
       { text: 'MultiSort', target: '.e-headercontent', id: 'MultiSort' }
     ]
-    this.editing = { allowDeleting: true, allowEditing: true, allowAdding: true, mode: 'Row' };
+    this.editing = { allowDeleting: true, showDeleteConfirmDialog: true, allowEditing: true, allowAdding: true, mode: 'Cell' };
     this.editparams = {params: { format: 'n' }};
+    this.toolbar = ['Add', 'Edit', 'Delite', 'Update', 'Cancel'];
   }
 
-  checking(event: any) {
-    event.preventDefault();
-    console.log("open popup");
-  }
-
-  openContext(event: any) {
-    event.preventDefault();
-    console.log('open context menu')
+  public initilaizeTarget: EmitType<object> = () => {
+    this.targetElement = this.container.nativeElement.parentElement;
   }
 
   contextMenuOpen(arg?: any) {
-    //(this.treegrid.getColumnByField('TaskID')
+    console.log('contextMenuOpen')
   }
 
   contextMenuClick(arg?: any) {
+    console.log(arg.column.field);
+    this.ejDialog.show();
+    //this.ejDialog.hide();
     //console.log(arg.item.properties.id); // get type of action
     //console.log(arg.column.field); // get colomn field name
     //console.log(arg.column.headerText); // get headerText
+
+    this.editCol(arg.column.field, arg.item.properties.id);
+
+
     const column = this.treegrid.getColumnByField(arg.column.field);
-    column.headerText = "Changed Text";
+    //column.headerText = "Changed Text";
     this.treegrid.refreshColumns();
+  }
+
+  public onOpenDialog = (event: any): void => {
+    console.log('onOpenDialog')
+  }
+
+  closeDialog(arg?: any) {
+    console.log(arg)
+    //console.log(arg.target.innerText)
+  }
+
+  buttonWasClicked(arg?: any) {
+    console.log(arg.target.innerText)
+    //this.ejDialog.hide();
+    //console.log('buttonWasClicked', arg)
+  }
+
+  editCol(columnName: string, action: string) {
+    console.log(columnName, action);
+    //console.log(this.taskForm);
+    //console.log('columnName', columnName);
+    //console.log('action', action);
+    const column = this.treegrid.getColumnByField(columnName);
+    this.formControl.setValue(column.headerText);
+    //column.headerText = "Changed Text";
+    this.treegrid.refreshColumns();
+  }
+
+  changeColName() {
+    //alert('changeColName')
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ButtonModule, CheckBoxAllModule } from '@syncfusion/ej2-angular-buttons';
 import { DialogModule } from '@syncfusion/ej2-angular-popups';
+import { DropDownListAllModule } from '@syncfusion/ej2-angular-dropdowns';
 import { ContextMenuService, FilterService, PageService, SortService, TreeGridModule, EditService, ToolbarService } from '@syncfusion/ej2-angular-treegrid';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NumericTextBoxAllModule } from '@syncfusion/ej2-angular-inputs';
+import { DatePickerAllModule } from '@syncfusion/ej2-angular-calendars';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,14 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     TreeGridModule,
-    DialogModule
+    DialogModule,
+    ButtonModule,
+    DropDownListAllModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NumericTextBoxAllModule,
+    CheckBoxAllModule,
+    DatePickerAllModule
   ],
   bootstrap: [AppComponent],
   providers: [PageService,
